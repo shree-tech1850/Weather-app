7e8ef10161a380dbce29cd220415f10fb45453a2
shree.dev <283730897+shree-tech1850@users.noreply.github.com>
2026-09-04 16:53:58 +0530

    Display Weather data

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const searchButton = document.getElementById("search-button");
 const cityInput = document.getElementById("city-input");
 
+const cityName = document.getElementById("city-name");
+const temperature = document.getElementById("temperature");
+const condition = document.getElementById("condition");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
+
 searchButton.addEventListener("click", async function () {
     const city = cityInput.value;
 
@@ -13,6 +19,15 @@ searchButton.addEventListener("click", async function () {
     const latitude = locationData.results[0].latitude;
     const longitude = locationData.results[0].longitude;
 
-    console.log("Latitude:", latitude);
-    console.log("Longitude:", longitude);
+    const weatherResponse = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m`
+    );
+
+    const weatherData = await weatherResponse.json();
+
+    cityName.textContent = city;
+    temperature.textContent = `${weatherData.current.temperature_2m}°C`;
+    humidity.textContent = `Humidity: ${weatherData.current.relative_humidity_2m}%`;
+    wind.textContent = `Wind: ${weatherData.current.wind_speed_10m} km/h`;
+    condition.textContent = "Weather data received!";
 });
